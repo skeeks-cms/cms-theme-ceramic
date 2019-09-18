26bcd77f26766bc843bcde2503bbc9a22049d8e5
Nelya-home <nelya@rusoft.ru>
2019-09-18 16:39:17 +0300

    Выводим примененные фильтры в списке коллекций

--- src/views/modules/cms/tree/collection.php
+++ src/views/modules/cms/tree/collection.php
@@ -131,7 +131,7 @@ CSS
 
                                 <?= $this->render('@app/views/filters/sorts', [
                                     'filtersWidget'              => $filtersWidget,
-                                    'sortFiltersHandler'         => $sortFiltersHandler,
+                                    'sortFiltersHandler'         => null,
                                 ]); ?>
 
                                 <?
