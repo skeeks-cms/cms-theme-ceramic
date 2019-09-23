71b1201fbe4e17b537f9f8bcf3fe42f895fb038f
Nelya-home <nelya@rusoft.ru>
2019-09-23 14:56:30 +0300

    Изменение внешнего вида коллекций. Добавление настройки отображения шаблона.

---
+++ src/views/widgets/ContentElementsCmsWidget/collection-block-item-v1.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010 SkeekS (СкикС)
+ * @date 06.03.2015
+ *
+ * @var skeeks\cms\themes\ceramic\models\CollectionCmsContentElement $model
+ *
+ */
+/* @var $this yii\web\View */
+
+$priceHelper = false;
+if ($model->minPriceProduct) {
+    $priceHelper = \Yii::$app->shop->cart->getProductPriceHelper($model->minPriceProduct);
+}
+?>
+<div class="g-brd-gray-light-v4 g-color-gray-dark-v2 g-brd-around g-bg-white">
+    <a class="tile__image-link" href="<?= $model->url; ?>">
+        <? if ($model->image) : ?>
+            <img class="img-fluid" src="<?= \Yii::$app->imaging->thumbnailUrlOnRequest($model->image ? $model->image->src : null,
+                new \skeeks\cms\components\imaging\filters\Thumbnail([
+                    'w' => 306,
+                    'h' => 206,
+                    //'m' => \Imagine\Image\ImageInterface::THUMBNAIL_INSET,
+                ]), $model->code
+            ); ?>" title="<?= \yii\helpers\Html::encode($model->name); ?>" alt="<?= \yii\helpers\Html::encode($model->name); ?>">
+        <? else : ?>
+            <img class="img-fluid g-transform-scale-1_1--parent-hover g-transition-0_5 g-transition--ease-in-out" src="<?= \skeeks\cms\helpers\Image::getCapSrc(); ?>" alt="<?= $model->name; ?>">
+        <? endif; ?>
+    </a>
+    <div class="g-pt-15 g-px-15">
+        <p>
+            <a class="h5" href="<?= $model->url; ?>"><?= $model->name; ?></a>
+        </p>
+        <div class="row g-height-50">
+            <div class="col-sm-6 col-6"><a class="tile__brand" href="<?= $model->url; ?>"><?= $model->brand; ?></a></div>
+            <div class="col-sm-6 col-6 text-sm-right"><span class="tile__country"><?= $model->country; ?></span></div>
+        </div>
+        <hr class="g-brd-gray-light-v4 g-py-5 g-my-0">
+        <div class="text-center h6 g-pb-10">Товаров: <?= $model->getProducts()->count(); ?>шт.</div>
+    </div>
+</div>
