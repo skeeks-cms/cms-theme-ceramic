c57c992e8fe8228692402f4471795952e896dee8
Semenov <semenov@skeeks.com>
2020-05-05 15:16:28 +0300

    dev

--- src/components/CeramicComponent.php
+++ src/components/CeramicComponent.php
@@ -5,25 +5,18 @@
  * @license https://cms.skeeks.com/license/
  * @author Semenov Alexander <semenov@skeeks.com>
  */
+
 namespace skeeks\cms\themes\ceramic\components;
+
 use skeeks\cms\base\Component;
-use skeeks\cms\components\Cms;
 use skeeks\cms\models\CmsAgent;
 use skeeks\cms\models\CmsContent;
-use skeeks\cms\models\CmsContentProperty;
-use skeeks\cms\models\CmsUser;
-use skeeks\cms\shop\models\ShopCart;
-use skeeks\cms\shop\models\ShopOrderStatus;
 use skeeks\cms\shop\models\ShopPersonType;
 use skeeks\cms\shop\models\ShopTypePrice;
 use skeeks\yii2\form\fields\BoolField;
 use skeeks\yii2\form\fields\FieldSet;
-use skeeks\yii2\form\fields\HtmlBlock;
 use skeeks\yii2\form\fields\SelectField;
-use skeeks\yii2\form\fields\TextareaField;
 use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
 /**
  * @author Semenov Alexander <semenov@skeeks.com>
  *
@@ -32,7 +25,6 @@ use yii\widgets\ActiveForm;
  * @property ShopTypePrice[]  $shopTypePrices
  * @property ShopTypePrice[]  $canBuyTypePrices
  *
- * @property ShopCart         $cart
  *
  * @property CmsContent       $shopContents
  * @property CmsContent       $storeContent
@@ -97,7 +89,7 @@ class CeramicComponent extends Component
                 ],
                 'boolean',
             ],
-            ['collection_item_view', 'string']
+            ['collection_item_view', 'string'],
         ]);
     }
 
@@ -106,36 +98,36 @@ class CeramicComponent extends Component
         return [
             'main' => [
                 'class' => FieldSet::class,
-                'name' => \Yii::t('skeeks/shop/app', 'Main'),
+                'name'  => \Yii::t('skeeks/shop/app', 'Main'),
 
                 'fields' => [
 
-                    'is_show_popular_collection' => [
-                        'class' => BoolField::class,
-                        'allowNull' => false,
+                    'is_show_popular_collection'  => [
+                        'class'       => BoolField::class,
+                        'allowNull'   => false,
                         'formElement' => BoolField::ELEMENT_RADIO_LIST,
                     ],
-                    'is_show_new_collection' => [
-                        'class' => BoolField::class,
-                        'allowNull' => false,
+                    'is_show_new_collection'      => [
+                        'class'       => BoolField::class,
+                        'allowNull'   => false,
                         'formElement' => BoolField::ELEMENT_RADIO_LIST,
                     ],
                     'is_show_home_modern_catalog' => [
-                        'class' => BoolField::class,
-                        'allowNull' => false,
+                        'class'       => BoolField::class,
+                        'allowNull'   => false,
                         'formElement' => BoolField::ELEMENT_RADIO_LIST,
                     ],
-                    'is_show_home_old_catalog' => [
-                        'class' => BoolField::class,
-                        'allowNull' => false,
+                    'is_show_home_old_catalog'    => [
+                        'class'       => BoolField::class,
+                        'allowNull'   => false,
                         'formElement' => BoolField::ELEMENT_RADIO_LIST,
                     ],
-                    'is_show_home_slider' => [
-                        'class' => BoolField::class,
-                        'allowNull' => false,
+                    'is_show_home_slider'         => [
+                        'class'       => BoolField::class,
+                        'allowNull'   => false,
                         'formElement' => BoolField::ELEMENT_RADIO_LIST,
                     ],
-                    'collection_item_view' => [
+                    'collection_item_view'        => [
                         'class' => SelectField::class,
                         'items' => [
                             'v1' => 'Вариант 1 (маленькие блоки как на tesser.ru)',
@@ -152,12 +144,12 @@ class CeramicComponent extends Component
     {
         return ArrayHelper::merge(parent::attributeLabels(), [
                 'is_show_popular_collection' => 'Показывать слайдер с популярными коллекциями на главной?',
-                'is_show_new_collection' => 'Показывать слайдер с новыми коллекциями на главной?',
-                'collection_item_view'             => "Вариант отображения коллекции",
+                'is_show_new_collection'     => 'Показывать слайдер с новыми коллекциями на главной?',
+                'collection_item_view'       => "Вариант отображения коллекции",
 
-                'is_show_home_modern_catalog'=> "Показывать современный каталог на главной",
-                'is_show_home_old_catalog'=> "Показывать обычный каталог на главной",
-                'is_show_home_slider'=> "Показывать слайдер на главной",
+                'is_show_home_modern_catalog' => "Показывать современный каталог на главной",
+                'is_show_home_old_catalog'    => "Показывать обычный каталог на главной",
+                'is_show_home_slider'         => "Показывать слайдер на главной",
             ]
         );
     }
